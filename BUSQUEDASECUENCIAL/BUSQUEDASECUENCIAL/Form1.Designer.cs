115baa7e04b7f0a5215c5764a1a2bdccb9fea859
German Ruiz <43076503+germanruzca@users.noreply.github.com>
2019-04-02 12:20:45 -0600

    Add files via upload

---
+++ BUSQUEDASECUENCIAL/BUSQUEDASECUENCIAL/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace BUSQUEDASECUENCIAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ALEATORIO = new System.Windows.Forms.Button();
+            this.buscar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LLENAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ALEATORIO
+            // 
+            this.ALEATORIO.Location = new System.Drawing.Point(153, 21);
+            this.ALEATORIO.Name = "ALEATORIO";
+            this.ALEATORIO.Size = new System.Drawing.Size(78, 23);
+            this.ALEATORIO.TabIndex = 1;
+            this.ALEATORIO.Text = "ALEATORIO";
+            this.ALEATORIO.UseVisualStyleBackColor = true;
+            this.ALEATORIO.Click += new System.EventHandler(this.ALEATORIO_Click);
+            // 
+            // buscar
+            // 
+            this.buscar.Location = new System.Drawing.Point(86, 50);
+            this.buscar.Name = "buscar";
+            this.buscar.Size = new System.Drawing.Size(75, 23);
+            this.buscar.TabIndex = 2;
+            this.buscar.Text = "BUSCAR";
+            this.buscar.UseVisualStyleBackColor = true;
+            this.buscar.Click += new System.EventHandler(this.buscar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 101);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 144);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(203, 25);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 225);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buscar);
+            this.Controls.Add(this.ALEATORIO);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ALEATORIO;
+        private System.Windows.Forms.Button buscar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ BUSQUEDASECUENCIAL/BUSQUEDASECUENCIAL/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BUSQUEDASECUENCIAL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] Vec = new int[10];
+        Random rnd = new Random();
+        int i,Num;
+        string Men,Men2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            for (i = 0; i < 10; i++)
+            {
+                Men = Microsoft.VisualBasic.Interaction.InputBox("Numero " + (i+1) + ": ");
+                Vec[i] = Convert.ToInt32(Men);
+                textBox1.Text = textBox1.Text + Vec[i] + ", ";
+            }
+            
+        }
+
+        private void ALEATORIO_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            for (i = 0; i < 10; i++)
+            {
+                Vec[i] = rnd.Next(0, 100);
+                textBox1.Text = textBox1.Text + Vec[i] + ", ";
+            }
+        }
+
+        private void buscar_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = " ";            
+            bool Op2=false;
+            Men2 = Microsoft.VisualBasic.Interaction.InputBox("Numero a buscar: ");
+            Num = Convert.ToInt32(Men2);
+            for (i = 0; i < 10; i++)
+            {
+                if (Vec[i]==Num)                
+                    Op2 = true;  
+            }
+            if (Op2 == true)            
+                textBox2.Text = "Numero encontrado!";            
+            else
+                textBox2.Text = "Numero no encontrado.";
+        }
+    }
+}
