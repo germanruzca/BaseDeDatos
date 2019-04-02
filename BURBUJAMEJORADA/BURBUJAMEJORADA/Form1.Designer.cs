cbde30f92362834ab3f31b073e334e143b05dea9
German Ruiz <43076503+germanruzca@users.noreply.github.com>
2019-04-02 12:18:16 -0600

    Add files via upload

---
+++ BURBUJAMEJORADA/BURBUJAMEJORADA/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace BURBUJAMEJORADA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.llenar = new System.Windows.Forms.Button();
+            this.ordenar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // llenar
+            // 
+            this.llenar.Location = new System.Drawing.Point(14, 24);
+            this.llenar.Name = "llenar";
+            this.llenar.Size = new System.Drawing.Size(88, 34);
+            this.llenar.TabIndex = 0;
+            this.llenar.Text = "LLENAR";
+            this.llenar.UseVisualStyleBackColor = true;
+            this.llenar.Click += new System.EventHandler(this.llenar_Click);
+            // 
+            // ordenar
+            // 
+            this.ordenar.Location = new System.Drawing.Point(137, 24);
+            this.ordenar.Name = "ordenar";
+            this.ordenar.Size = new System.Drawing.Size(88, 34);
+            this.ordenar.TabIndex = 1;
+            this.ordenar.Text = "ORDENAR";
+            this.ordenar.UseVisualStyleBackColor = true;
+            this.ordenar.Click += new System.EventHandler(this.ordenar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 162);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 237);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ordenar);
+            this.Controls.Add(this.llenar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button llenar;
+        private System.Windows.Forms.Button ordenar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ BURBUJAMEJORADA/BURBUJAMEJORADA/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BURBUJAMEJORADA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] Vec = new int[10];
+        Random rnd = new Random();
+        int j, i, aux;
+        bool Op = true;
+        private void ordenar_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i < 10; i++)
+            {
+                Op = false;
+                for (j = 0; j < 9; j++)
+                {
+                    if (Vec[j] > Vec[j + 1])
+                    {
+                        aux = Vec[j];
+                        Vec[j] = Vec[j + 1];
+                        Vec[j + 1] = aux;
+                        Op = true;
+                    }                                      
+                }
+                if (Op == false)
+                    break;
+            }
+            textBox2.Text = " ";
+            for (i = 0; i < 10; i++)
+            {
+                textBox2.Text = textBox2.Text + Vec[i] + ", ";
+            }
+        }    
+        private void llenar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            for (i = 0; i < 10; i++)
+            {
+                Vec[i] = rnd.Next(1, 100);
+            }
+            for (i = 0; i < 10; i++)
+            {
+                textBox1.Text = textBox1.Text + Vec[i] + ", ";
+            }
+        }
+    }
+}
